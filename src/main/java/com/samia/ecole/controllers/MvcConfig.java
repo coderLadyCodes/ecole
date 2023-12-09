258aa3baab36bde23c4c00600b5761a5bc07055c
coderLady <creativegirl@outlook.fr>
2023-12-09 21:22:05 +0100

    fucking image upload is a mess

--- src/main/java/com/samia/ecole/controllers/MvcConfig.java
+++ src/main/java/com/samia/ecole/controllers/MvcConfig.java
@@ -2,12 +2,13 @@ package com.samia.ecole.controllers;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Override
