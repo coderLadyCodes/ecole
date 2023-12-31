19082ff815cbde83047fbd67bc266abfd74cd37a
coderLady <creativegirl@outlook.fr>
2023-12-31 18:06:52 +0100

    multipartFile working fine

--- src/main/java/com/samia/ecole/controllers/UserController.java
+++ src/main/java/com/samia/ecole/controllers/UserController.java
@@ -28,22 +28,38 @@ public class UserController {
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }) //produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE
+    public UserDTO createUser(@RequestPart UserDTO userDTO, @RequestPart MultipartFile multipartFile) throws IOException {
 
-    @PostMapping(consumes = "multipart/mixed") //consumes = "multipart/mixed"   consumes=MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
-    public UserDTO createUser(@RequestBody UserDTO userDTO, @RequestPart(value="profileImage",required = false) MultipartFile multipartFile) throws IOException, IOException {
-        System.out.println("START");
         if(multipartFile == null){
-                return userService.createUser(userDTO);
-            }else {
-            System.out.println("FILE EXISTS");
+            return userService.createUser(userDTO);
+        }else {
+
             String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
             userDTO.setProfileImage(fileName);
             UserDTO savedUser = userService.createUser(userDTO);
             String uploadDir = "images/" + savedUser.getId();
             FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-            System.out.println("THE OUTPUT IS LIKELY TO BE : " + uploadDir + fileName + multipartFile);
+
             return savedUser;
-    }}
+        }
+    }
+
+//    @PostMapping(consumes=MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE) //consumes = "multipart/mixed"
+//    public UserDTO createUser(@RequestBody UserDTO userDTO, @RequestPart(value="profileImage",required = false) MultipartFile multipartFile) throws IOException, IOException {
+//        System.out.println("START");
+//        if(multipartFile == null){
+//                return userService.createUser(userDTO);
+//            }else {
+//            System.out.println("FILE EXISTS");
+//            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+//            userDTO.setProfileImage(fileName);
+//            UserDTO savedUser = userService.createUser(userDTO);
+//            String uploadDir = "images/" + savedUser.getId();
+//            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+//            System.out.println("THE OUTPUT IS LIKELY TO BE : " + uploadDir + fileName + multipartFile);
+//            return savedUser;
+//    }}
 //    @PostMapping()
 //    public UserDTO createUser(@RequestBody UserDTO userDTO){
 //        return userService.createUser(userDTO);
